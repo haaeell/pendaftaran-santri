eefdbba57d09102393cf9a788e876ade3613ca43
haaeell <haikaljawahir434@gmail.com>
2025-12-04 23:13:26 +0700

    setup migration

---
+++ app/Models/HasilTesSantri.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HasilTesSantri extends Model
+{
+    protected $table = 'hasil_tes_santri';
+
+    protected $fillable = [
+        'user_id',
+        'kategori_id',
+        'nilai',
+        'lulus_threshold',
+    ];
+
+    protected $casts = [
+        'lulus_threshold' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function kategori()
+    {
+        return $this->belongsTo(KategoriSoal::class, 'kategori_id');
+    }
+}
